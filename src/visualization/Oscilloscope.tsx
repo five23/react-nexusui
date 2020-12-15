3f855375e2432084178d75902f85fb5ba56f42f2
David Kirby <david@ecomatics.net>
2020-12-15 12:39:21 -0500

    Testing

---
+++ src/visualization/Oscilloscope.tsx
@@ -0,0 +1,26 @@
+import * as React from "react";
+//@ts-ignore
+import * as Nexus from "nexusui";
+import { OscilloscopeProps, OscilloscopeChange } from "../types";
+import { getId } from "../utils";
+
+export const Oscilloscope = React.memo(function Oscilloscope({
+  size = [120, 30],
+}: OscilloscopeProps) {
+  let osc = React.useRef<null | Nexus.Oscilloscope>(null);
+  let elementId = React.useRef(`nexus-ui-pan-${getId()}`);
+  React.useEffect(() => {
+    osc.current = new Nexus.Oscilloscope(elementId.current, { size });
+    return () => {
+      osc.current.destroy();
+    };
+  }, []);
+  React.useEffect(() => {
+    if (osc.current === null) return;
+    if (!Array.isArray(size)) {
+      return;
+    }
+    osc.current.resize(...size);
+  }, size);
+  return <div id={elementId.current} />;
+});
