962c682523102b9ec008b680812124621b275a9c
David Kirby <david@ecomatics.net>
2020-12-15 18:52:11 -0500

    Remove dist

--- dist/general/Multislider.d.ts
+++
@@ -1,3 +0,0 @@
-import React from "react";
-import { MultisliderProps } from "../types";
-export declare const Multislider: React.NamedExoticComponent<MultisliderProps>;
--- dist/visualization/Oscilloscope.d.ts
+++
@@ -1,3 +0,0 @@
-import * as React from "react";
-import { OscilloscopeProps } from "../types";
-export declare const Oscilloscope: React.NamedExoticComponent<OscilloscopeProps>;
